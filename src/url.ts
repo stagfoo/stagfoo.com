575afa72f40af4c300654f64f2cbdebe063cab9a
stagfoo <alexanderddking@@gmail.com>
2021-12-27 00:19:35 +0900

    🪴　feature ░ Add background to url

--- src/url.ts
+++ src/url.ts
@@ -1,23 +1,52 @@
 import page from 'page';
 import { state } from './index';
-import { hideNotifications, showNotifications } from './actions';
+import { BaseStyles, STYLES  } from './styles';
 
 // Handlers
 const HOME_PAGE = (ctx, next) => {
-  state._update('updateCurrentPage', 'HOME')
+  state._update('updateCurrentPage', 'home')
+  state._update('updateBackground', { text:"( ･3･)～", n: 1000 })
 };
-const EXAMPLE_FETCH = (ctx, next) => {
-  getData(window.location.origin + "/data.json").then(data => {
-    state._update('updateBucket', data.greeting)
-    state._update('updateCurrentPage', 'EXAMPLE_FETCH')
-    showNotifications("Shark data loaded  (´ε｀ )♡")
-    hideNotifications(1000)
-  })
+const JORO = (ctx, next) => {
+  BaseStyles();
+  state._update('updateBackground', { text:"スパイダー", n: 1000 })
+  if(ctx.querystring == "delete-all-css"){
+    STYLES.remove('baseStyles', true);
+    state._update('updateBackground', { text:"Congrats, you deleted all css added by Joro", n: 1 })
+  }
+  state._update('updateCurrentPage', 'joro')
+};
+const CHUMBUCKET = (ctx, next) => {
+  state._update('updateCurrentPage', 'chumbucket')
+  state._update('updateBackground', { text:"", n: 0 })
+
+};
+const OBAKE = (ctx, next) => {
+  state._update('updateCurrentPage', 'obake')
+  state._update('updateBackground', { text:"(ﾉ⊙﹏⊙)ﾉ", n: 1000 })
+};
+const DEATHMARK = (ctx, next) => {
+  state._update('updateCurrentPage', 'deathmark')
+  state._update('updateBackground', { text:"", n: 0 })
+};
+const AMICO = (ctx, next) => {
+  state._update('updateCurrentPage', 'amico')
+  state._update('updateBackground', { text:"", n: 0 })
+};
+const LIBRARIES = (ctx, next) => {
+  BaseStyles();
+  state._update('updateCurrentPage', 'libraries')
+  state._update('updateBackground', { text: 'a list of links░ ', n: 1000 })
 };
 
 // Routes
 page('/', HOME_PAGE);
-page('/example-fetch', EXAMPLE_FETCH);
+page('/joro', JORO);
+page('/libraries', LIBRARIES);
+page('/chumbucket', CHUMBUCKET);
+page('/obake', OBAKE);
+page('/deathmark', DEATHMARK);
+page('/amico', AMICO);
 
 
 export function startRouters(): void {

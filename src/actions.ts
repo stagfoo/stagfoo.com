6398c3631fbc8c3c97bd9533422ba462b7dc0da9
stagfoo <alexanderddking@@gmail.com>
2021-12-27 09:22:11 +0900

    🪴　feature ░ Demo Buttons Obake, joro

--- src/actions.ts
+++ src/actions.ts
@@ -1,4 +1,5 @@
 import { state } from './index';
+import { STYLES, BaseStyles } from './styles';
 
 export function handleButtonClick() {
   state._update('updateBucket', state.bucket + '🍖')
@@ -19,3 +20,18 @@ export function showNotifications(message: string) {
     show: true
   })
 };
+
+export function handleJoroDemo(){
+  if(STYLES.registry['baseStyles']){
+    state._update('updateBackground', { text:"/////Congrats, you deleted all css added by Joro/////", n: 200 })
+    STYLES.remove('baseStyles', true);
+  } else {
+    BaseStyles()
+    state._update('updateBackground', { text:"/////Congrats, added css with Joro/////", n: 200 })
+  }
+}
+
+export function handleObakeDemo(){
+  state._update('updateCount', state.count+1);
+    state._update('updateBackground', { text:`/////You updated the state with obake.js ${state.count} times /////`, n: 200 })
+}
--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ export const defaultState = {
   bucket: '',
   currentPage: { name: 'home' },
   pages: {},
+  count: 0,
   background: { text:"( ･3･)～", n: 1000 },
   notification: {
     text: "",
@@ -24,16 +25,16 @@ export const reducers = {
   updateCurrentPage: reducer((state, value: string) => {
     state.currentPage = { name: value  };
   }),
+  updateCount: reducer((state, value: string) => {
+    state.count = value;
+  }),
   updateBucket: reducer((state, value: string) => {
     state.bucket = value;
   }),
   updatePages: reducer((state, value: string) => {
     state.pages = value;
   }),
-  updateBackground: reducer((state, value: string) => {
+  updateBackground: reducer((state, value) => {
     state.background = value;
-  }),
-  updateNotification: reducer((state, value:{text: string, show: boolean}) => {
-    state.notification = value;
   }),
 }
